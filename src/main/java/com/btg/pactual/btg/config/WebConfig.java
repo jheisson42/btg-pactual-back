6126eee4515cbb1dfe563050dcf9fe19905a7d6e
DESKTOP-32R16U6\{JSON} <jheisson42@gmail.com>
2024-10-03 12:34:23 -0500

    ajuste cors

--- src/main/java/com/btg/pactual/btg/config/WebConfig.java
+++ src/main/java/com/btg/pactual/btg/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 	   @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	            .allowedOrigins("https://angular-btg-pactual.s3.us-east-2.amazonaws.com")
+	            .allowedOrigins("https://angular-btg-pactual.s3.us-east-2.amazonaws.com", "http://localhost:4200/")
 	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
 	            .allowedHeaders("*")
 	            .allowCredentials(true);
